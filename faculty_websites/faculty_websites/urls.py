a84d7ec64f287f746b9865f9e00bf2bd7762fb8d
Brandon Lafving <52582453+blafving@users.noreply.github.com>
2023-04-02 14:35:38 -0400

    basic url conf

--- faculty_websites/faculty_websites/urls.py
+++ faculty_websites/faculty_websites/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('faculty/', include('faculty.urls'))
+    path('faculty/', include('faculty.urls')),
     path('admin/', admin.site.urls),
 ]
